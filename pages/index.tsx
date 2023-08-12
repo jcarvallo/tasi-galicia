d6b879a757f25e2a2d80525a70291ac8c3e2435d
jcarvallo <jcarvallo.ar@gmail.com>
2023-08-11 21:12:57 -0300

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,12 @@
-import { useStore } from "@/store/useStore";
 import { Button, Container } from "@mui/material";
 
 export default function Home() {
-  const { navigate } = useStore()();
-
-  function redirect() {
-    navigate.push("/operation");
-  }
+ 
   return (
     <>
       <Container>
         <h1>Tasi Galicia</h1>
-        <Button variant="contained" onClick={() => redirect()}>
+        <Button variant="contained">
           Operation
         </Button>
       </Container>
